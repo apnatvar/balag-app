235e68bb7ffada3d255adc064b68c43004f68708
apnatvar <arawat@tcd.ie>
2025-07-06 15:48:29 +0530

    added blogs succesfully

--- app/blog/[title]/page.jsx
+++ app/blog/[title]/page.jsx
@@ -1,8 +1,16 @@
 import dynamic from 'next/dynamic';
+import SideMenu from '@/components/sideMenu';
 
 const BlogPost = dynamic(() => import('@/components/blogTemplate'));
 
 export default async function Page({ params }) {
   const { title } = await params
-  return <BlogPost title={title} />
+  return (
+    <html lang="en">
+      <body>
+        <SideMenu />
+        <BlogPost title={title} />
+      </body>
+    </html>
+  )
 }
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import SideMenu from "@/components/sideMenu";
 
 export default function BlogPage() {
+  const [jsonData, setJsonData] = useState({});
 
-    
+  useEffect(() => {
+    fetch("http://localhost:5000/blog/")
+      .then((res) => res.json())
+      .then((data) => {
+        setJsonData(data);
+      })
+      .catch((err) => {
+        console.error("Failed to load JSON:", err);
+      });
+  }, []);
+
+  if (!jsonData.page) return <p>Loading...</p>;
+
   return (
     <html lang="en">
       <body>
-        <h1>Blog Page</h1>
-        <p>This is the blog page. You can add your blog posts here.</p>
+        <SideMenu />
+        <section className="blog-hero">
+          <h1 className="heading">{jsonData.page.heading}</h1>
+          <p className="subheading">{jsonData.page.subheading}</p>
+            <div className="blog-cards">
+              {generateBlogCards(jsonData.blogs)}
+            </div>
+        </section>
       </body>
     </html>
   );
+
+  function generateBlogCards(blogs) {
+    if (!blogs) return <p>Loading...</p>;
+
+    return Object.entries(blogs).map(([route, blog]) => (
+      <div className="card" key={route} >
+        <h2 className="title" >{blog.title}</h2>
+        <img
+          className="image"
+          loading="lazy"
+          src={blog.image}
+          alt={blog.alt}
+        />
+        <a className="description" href={`/blog/${route}`}>{blog.brief}</a>
+      </div>
+    ));
+  }
 }
