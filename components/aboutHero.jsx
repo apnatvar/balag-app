519ea22ca46aa9b126a765a1c8af0d4f2580a935
apnatvar <arawat@tcd.ie>
2025-07-23 20:50:15 +0530

    sanity changes

--- components/aboutHero.jsx
+++ components/aboutHero.jsx
@@ -5,7 +5,7 @@ export default function AboutHeroSection({heroContent}) {
     return (
         <section className="about-hero">
             <div className="overlay">
-                <img className="image" src={heroContent.image.url} alt={heroContent.image.alt}></img>
+                <img className="image" src={heroContent.image.url} alt={heroContent.image.alt} />
                 <img className="logo" src={heroContent.logo.url} alt={heroContent.logo.alt} />
                 <nav className="nav">
                     {generateNavButtons(heroContent.nav)}
